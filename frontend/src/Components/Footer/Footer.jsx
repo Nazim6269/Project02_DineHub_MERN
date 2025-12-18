a4c54132fd37c6522af10e474b7d2f795c7b30cc
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2025-12-18 09:30:42 +0600

    Update footer brand name from 'Dreams Food' to 'DineHub'

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-white-900">
-              <span className="ml-3 text-xl">Dreams Food</span>
+              <span className="ml-3 text-xl">DineHub</span>
             </a>
             <p className="mt-2 text-sm ">
               Air plant banjo lyft occupy retro adaptogen indego
